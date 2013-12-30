5ee45ede8cd9d7f3c5eff19ce4f4e12cc42a8c28
Jeffrey D. Hoffman <jeffrey@inkceptional.com>
2013-12-30 12:04:30 -0600

    Removed on-mobile attribute - use mobile exclusively.
    Supporting both on-mobile and mobile was a bit unnecessary.  No one is going to use the more verbose on-mobile when a shorter solution is available.

--- lib/inkcite/renderer/responsive.rb
+++ lib/inkcite/renderer/responsive.rb
@@ -1,88 +1,87 @@
 module Inkcite
-  module Responsive
+  module Renderer
+    module Responsive
 
-    # Constants
-    BUTTON  = 'button'
-    DROP    = 'drop'
-    FILL    = 'fill'
-    HIDE    = 'hide'
+      # Constants
+      BUTTON  = 'button'
+      DROP    = 'drop'
+      FILL    = 'fill'
+      HIDE    = 'hide'
 
-    # Attribute names
-    ON_MOBILE = :'on-mobile'
+      def css_rule tag, klass, declarations, selector=nil
 
-    def css_rule tag, klass, declarations, selector=nil
+        rule = ""
+        rule << tag
+        rule << "[class~=#{quote(klass)}]"
+        rule << " #{selector}" unless selector.blank?
+        rule << " { "
+        if declarations.is_a?(Hash)
+          rule << render_styles(declarations)
+        elsif declarations.is_a?(Array)
+          rule << declarations.join(' ')
+        else
+          rule << declarations.to_s
+        end
+        rule << " }"
 
-      rule = ""
-      rule << tag
-      rule << "[class~=#{quote(klass)}]"
-      rule << " #{selector}" unless selector.blank?
-      rule << " { "
-      if declarations.is_a?(Hash)
-        rule << render_styles(declarations)
-      elsif declarations.is_a?(Array)
-        rule << declarations.join(' ')
-      else
-        rule << declarations.to_s
-      end
-      rule << " }"
-
-      rule
-    end
-
-    def button? att
-      matches? att, BUTTON_MODES
-    end
-
-    # Returns true if the attribute value corresponds to the
-    # drop pattern.
-    def drop? att
-      matches? att, DROP_MODES
-    end
-
-    # Returns true if the attribute value corresponds to the
-    # fill or stretch pattern.
-    def fill? att
-      matches? att, FILL_MODES
-    end
-
-    # Returns true if the attribute value corresponds to the
-    # hide on mobile pattern.
-    def hide? att
-      matches? att, HIDE_MODES
-    end
-
-    def invalid_mode tag, mode, ctx
-      ctx.error "Invalid responsive mode for {#{tag}}", { ON_MOBILE => mode }
-    end
-
-    def matches? att, mode
-      mode.include?(att.to_s.downcase)
-    end
-
-    def responsive_mode opt
-
-      att = opt[ON_MOBILE] || opt[:mobile]
-      if button?(att)
-        BUTTON
-      elsif drop?(att)
-        DROP
-      elsif fill?(att)
-        FILL
-      elsif hide?(att)
-        HIDE
-      else
-        nil
+        rule
       end
 
+      def button? att
+        matches? att, BUTTON_MODES
+      end
+
+      # Returns true if the attribute value corresponds to the
+      # drop pattern.
+      def drop? att
+        matches? att, DROP_MODES
+      end
+
+      # Returns true if the attribute value corresponds to the
+      # fill or stretch pattern.
+      def fill? att
+        matches? att, FILL_MODES
+      end
+
+      # Returns true if the attribute value corresponds to the
+      # hide on mobile pattern.
+      def hide? att
+        matches? att, HIDE_MODES
+      end
+
+      def invalid_mode tag, mode, ctx
+        ctx.error "Invalid responsive mode for {#{tag}}", { :mobile => mode }
+      end
+
+      def matches? att, mode
+        mode.include?(att.to_s.downcase)
+      end
+
+      def responsive_mode opt
+
+        att = opt[:mobile]
+        if button?(att)
+          BUTTON
+        elsif drop?(att)
+          DROP
+        elsif fill?(att)
+          FILL
+        elsif hide?(att)
+          HIDE
+        else
+          nil
+        end
+
+      end
+
+      private
+
+      # Variants, aliases for convenience.
+      BUTTON_MODES = Set.new [ BUTTON ]
+      DROP_MODES   = Set.new [ DROP, 'stack' ]
+      FILL_MODES   = Set.new [ FILL, 'stretch' ]
+      HIDE_MODES   = Set.new [ HIDE, 'hidden' ]
+
     end
-
-    private
-
-    # Variants, aliases for convenience.
-    BUTTON_MODES = Set.new [ BUTTON ]
-    DROP_MODES   = Set.new [ DROP, 'stack' ]
-    FILL_MODES   = Set.new [ FILL, 'stretch' ]
-    HIDE_MODES   = Set.new [ HIDE, 'hidden' ]
-
   end
 end
