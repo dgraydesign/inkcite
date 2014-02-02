eb5f4f1434cd855c4fa439d9c52b74a57038358b
Jeffrey D. Hoffman <jeffrey@inkceptional.com>
2014-02-02 16:24:56 -0600

    Some extra commenting regarding lazy loading and using Lorem requires an error.

--- lib/inkcite/renderer/lorem.rb
+++ lib/inkcite/renderer/lorem.rb
@@ -2,8 +2,9 @@ module Inkcite
   module Renderer
     class Lorem < Base
 
-      def render(tag, opt, ctx)
+      def render tag, opt, ctx
 
+        # Lazy load only if Lorem is used in the email.
         require 'faker'
 
         type = (opt[:type] || :sentences).to_sym
@@ -11,6 +12,8 @@ module Inkcite
         # Get the limit (e.g. the number of sentences )
         limit = opt[:sentences] || opt[:size] || opt[:limit] || opt[:count]
 
+        # Always warn the creator that there is Lorem Ipsum in the email because
+        # we don't want it to ship accidentally.
         ctx.error 'Email contains Lorem Ipsum'
 
         if type == :headline
