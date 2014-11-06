1e90cf6fe8d64f012bccb756536b0eff09dc9d0d
Jeffrey D. Hoffman <jeffrey@inkceptional.com>
2014-11-06 09:26:40 -0600

    Fixed an encoding problem on email upload
    The Net::SFTP gem uses ASCII-8BIT when uploading ascii files.  If the email contained UTF-8 characters, the upload would fail.  Instead, the email is written to a stringio buffer and that is binary uploaded to the server which honors encoding.

--- lib/inkcite/uploader.rb
+++ lib/inkcite/uploader.rb
@@ -25,11 +25,53 @@ module Inkcite
       # Determine when the last upload was completed.
       last_upload = email.meta(:last_upload).to_i
 
-      self.upload!(email) if last_update > last_upload
+      self.do_upload(email, false) if last_update > last_upload
 
     end
 
     def self.upload! email
+      self.do_upload(email, true)
+    end
+
+    private
+
+    IMAGE_PATH = :'image-path'
+
+    def self.copy! sftp, local, remote, force=true
+
+      # Nothing to copy unless the local directory exists (e.g. some emails don't
+      # have an images directory.)
+      return unless File.exists?(local)
+
+      Dir.foreach(local) do |file|
+        next if file.starts_with?('.')
+
+        local_file = File.join(local, file)
+        unless File.directory?(local_file)
+
+          remote_file = File.join(remote, file)
+
+          unless force
+            next unless begin
+              File.stat(local_file).mtime > Time.at(sftp.stat!(remote_file).mtime)
+            rescue Net::SFTP::StatusException
+              true # File doesn't exist, so assume it's changed.
+            end
+          end
+
+          puts "Uploading #{local_file} -> #{remote_file} ..."
+          sftp.upload!(local_file, remote_file)
+
+        end
+
+      end
+    end
+
+    # Internal method responsive for doing the actual upload and
+    # forcing (if necessary) the update of the graphics.
+    def self.do_upload email, force
+
+      require 'stringio'
 
       # The preview version defines the configuration for the server to which
       # the files will be sftp'd.
@@ -41,8 +83,18 @@ module Inkcite
       username = config[:username]
       password = config[:password]
 
+      # Pre-optimize images before we upload them to the CDN.
+      email.optimize_images
+
       # This is the directory from which images will be uploaded.
-      local_images = email.image_dir
+      # The email provides us with the correct directory based on
+      # whether or not image optimization is enabled.
+      local_images = email.optimized_image_dir
+
+      # This is the last location of image upload.  If we're working
+      # on multiple versions but the images all point to the same
+      # location, it isn't necessary to re-upload images each time.
+      last_remote_root = nil
 
       puts "Uploading to #{host} ..."
 
@@ -67,11 +119,24 @@ module Inkcite
           # HTML version of the email - but this is a bad practice.
           file_name = view.file_name
           unless file_name.blank?
-            sftp.file.open(File.join(remote_root, file_name), "w") { |f| view.write(f) }
+
+            remote_file_name = File.join(remote_root, file_name)
+            puts "Uploading #{remote_file_name}"
+
+            # We need to use StringIO to write the email to a buffer in order to upload
+            # the email's content in binary so that its encoding is honored.  SFTP defaults
+            # to ASCII-8bit in non-binary mode, so it was blowing up on UTF-8 special
+            # characters (e.g. "Mäkinen").
+            # http://stackoverflow.com/questions/9439289/netsftp-transfer-mode-binary-vs-text
+            io = StringIO.new
+            view.write(io)
+            sftp.upload!(io, remote_file_name)
+
           end
 
-          # Upload the images to the remote directory.
-          copy! sftp, local_images, remote_root
+          # Upload the images to the remote directory
+          copy! sftp, local_images, remote_root, force && last_remote_root != remote_root
+          last_remote_root = remote_root
 
         end
 
@@ -81,38 +146,6 @@ module Inkcite
       email.set_meta :last_upload, Time.now.to_i
 
       true
-    end
-
-    private
-
-    IMAGE_PATH = :'image-path'
-
-    def self.copy! sftp, local, remote
-
-      # Nothing to copy unless the local directory exists (e.g. some emails don't
-      # have an images directory.)
-      return unless File.exists?(local)
-
-      Dir.foreach(local) do |file|
-        next if file.starts_with?('.')
-
-        local_file = File.join(local, file)
-        unless File.directory?(local_file)
-
-          remote_file = File.join(remote, file)
-
-          next unless begin
-            File.stat(local_file).mtime > Time.at(sftp.stat!(remote_file).mtime)
-          rescue Net::SFTP::StatusException
-            true # File doesn't exist, so assume it's changed.
-          end
-
-          puts "Uploading #{local_file} -> #{remote_file} ..."
-          sftp.upload!(local_file, remote_file)
-
-        end
-
-      end
     end
 
     def self.mkdir! sftp, path
