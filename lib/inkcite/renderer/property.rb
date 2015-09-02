9656eb1b30305e974cb93def5ee23a47b0be8d23
Jeffrey D. Hoffman <jeffrey@inkceptional.com>
2015-09-01 22:40:00 -0500

    Use the tag stack for custom helpers, keeping closing tags DRY
    It's bugged me for a long time that custom helpers (e.g. {feature ...}) didn't use the tag stack.  If you had a variable used in both the open and closing HTML, you had to repeat it in the closing tag (e.g. {feature name=value} and {/feature name=value}).  Y'all know how I feel about repeating yourself in HTML.  Here there be dragons.
    
    So now the custom Property (a.k.a. custom helper) renderer determines if the tag is an open or close tag and uses the tag stack accordingly.

--- lib/inkcite/renderer/property.rb
+++ lib/inkcite/renderer/property.rb
@@ -10,6 +10,33 @@ module Inkcite
           return nil
         end
 
+        # True if this is a opening tag - e.g. {feature ...}, not {/feature}
+        is_open_tag = !tag.starts_with?(SLASH)
+
+        # When a custom opening tag is encountered, if there is a corresponding
+        # closing tag, we'll save the options provided at open so that they
+        # can be pop'd by the closing tag and used in the closing HTML.
+        if is_open_tag
+
+          # Verify that a closing tag has been defined and push the opts
+          # onto the stack.  No need to push opts and pollute the stack if
+          # there is no closing tag to take advantage of them.
+          close_tag = "#{SLASH}#{tag}"
+          ctx.tag_stack(tag) << opt unless ctx[close_tag].blank?
+
+        else
+
+          # Chop off the forward slash to reveal the original open tag.  Then
+          # grab the tag stack for said open tag.  Pop the most recently provided
+          # opts off the stack so those values are available again.
+          open_tag = tag[1..-1]
+          tag_stack = ctx.tag_stack(tag[1..-1])
+
+          # The provided opts take precedence over the ones passed to the open tag.
+          opt = tag_stack.pop.merge(opt)
+
+        end
+
         # Need to clone the property - otherwise, we modify the original property.
         # Which is bad.
         html = html.clone
@@ -33,6 +60,7 @@ module Inkcite
 
       DOLLAR = '$'
       EQUALS = '='
+      SLASH = '/'
 
     end
   end
