48006080dcc7454be9643e5295d333068428bbff
Jeffrey D. Hoffman <jeffrey@inkceptional.com>
2015-11-07 07:19:14 -0600

    Div Helper now supports width, valign and display attributes
    In preparation for supporting Fluid-Drop, the Div Helper needed to support width, vertical-align and display attributes.  These are translated into the appropriate CSS styles.

--- lib/inkcite/renderer/div.rb
+++ lib/inkcite/renderer/div.rb
@@ -8,16 +8,24 @@ module Inkcite
 
         div = Element.new('div')
 
+        width = opt[:width]
+        div.style[:width] = px(width) unless width.blank?
+
         height = opt[:height].to_i
         div.style[:height] = px(height) if height > 0
+
+        mix_background div, opt
+        mix_font div, opt, ctx
 
         # Text alignment - left, right, center.
         align = opt[:align]
         div.style[TEXT_ALIGN] = align unless none?(align)
 
-        mix_font div, opt, ctx
+        valign = opt[:valign]
+        div.style[VERTICAL_ALIGN] = valign unless valign.blank?
 
-        mix_background div, opt
+        display = opt[:display]
+        div.style[:display] = display unless display.blank?
 
         mix_responsive div, opt, ctx
 
