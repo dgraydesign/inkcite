809c677325dde186098cdb7b6f7eb5ab7ee05ce9
Jeffrey D. Hoffman <jeffrey@inkceptional.com>
2016-02-03 05:43:54 -0600

    General improvements to the Snow Helper
    Moved the static constants below the main render method, made them private.  Removed the randomness to the initial timing as it was leading to periods of initial emptiness.  When combined with a new approach to random initial start position, the overall feeling is more pleasant. Added a spread Range.  Fixed a problem where sufficiently large snow flakes could appear within the container.
    
    General code cleanup and improvements.

--- lib/inkcite/renderer/snow.rb
+++ lib/inkcite/renderer/snow.rb
@@ -2,20 +2,6 @@ module Inkcite
   module Renderer
     class Snow < ContainerBase
 
-      # Size constraints on the flakes.
-      FLAKE_SIZE_MIN = :'min-size'
-      FLAKE_SIZE_MAX = :'max-size'
-
-      # Speed constraints on the flakes.
-      FLAKE_SPEED_MIN = :'min-speed'
-      FLAKE_SPEED_MAX = :'max-speed'
-
-      # Opacity constraints.
-      FLAKE_OPACITY_MIN = :'min-opacity'
-      FLAKE_OPACITY_MAX = :'max-opacity'
-      OPACITY_FLOOR = 0.2
-      OPACITY_CEIL = 1.0
-
       # Ambient snow special effect renderer courtesy of
       # http://freshinbox.com/blog/ambient-animations-in-email-snow-and-stars/
       def render tag, opt, ctx
@@ -47,7 +33,7 @@ module Inkcite
         # the variance between where the flake starts and where it ends.
         # Measured in %-width of the overall area.
         spread = (opt[:spread] || 20).to_i
-        half_spread = spread / 2.0;
+        half_spread = spread / 2.0
 
         # Determine the opacity variance.
         flake_min_opacity = (opt[FLAKE_OPACITY_MIN] || 0.5).to_f
@@ -57,9 +43,9 @@ module Inkcite
         end_time = (opt[:time] || 4).to_f
 
         # Setup some ranges for easier random numbering.
-        start_time_range = (0.0...end_time)
         size_range = (flake_min_size..flake_max_size)
         speed_range = (flake_min_speed..flake_max_speed)
+        spread_range = (-half_spread..half_spread)
         opacity_range = (flake_min_opacity..flake_max_opacity)
 
         # Snowflake color.
@@ -109,16 +95,35 @@ module Inkcite
         # Common attributes for all snowflakes.
         style << "  .#{all_flakes_class} {"
         style << '    position: absolute;'
-        style << '    top: -20px;'
+        style << "    top: -#{flake_max_size + 4}px;"
         style << "    background-color: #{color};"
         style << '  }'
+
+        # Space the snowflakes generally equally across the width of the
+        # container div.  Random distribution sometimes ends up with
+        # snowflakes clumped at one edge or the other.
+        flake_spacing = 100 / flakes.to_f
+
+        # Now build up a pool of equally-spaced starting positions.
+        # TODO: This is probably a perfect spot to use inject()
+        start_left = flake_spacing / 2.0
+        start_positions = [ start_left ]
+        (flakes - 1).times { |f| start_positions << start_left += flake_spacing }
+
+        # Randomize the starting positions - otherwise they draw right-to-left
+        # as starting positions are popped from the pool.
+        start_positions.shuffle!
+
+        # Snowflakes will be dispersed equally across the total time
+        # of the animation making for a smoother, more balanced show.
+        start_interval = end_time / flakes.to_f
+        start_time = 0
 
         # Now add individual class definitions for each flake with unique size,
         # speed and starting position.  Also add the animation trigger that loops
         # infinitely, starts at a random time and uses a random speed to completion.
         flakes.times do |flake|
 
-          start_time = rand(start_time_range).round(1)
           speed = rand(speed_range).round(1)
           size = rand(size_range)
 
@@ -134,27 +139,31 @@ module Inkcite
           style << "    width: #{px(size)};"
           style << "    border-radius: #{px((size / 2.0).round)};"
           style << "    opacity: #{opacity};" if opacity < OPACITY_CEIL
-          style << "    -webkit-animation: #{anim_prefix}#{flake + 1} #{speed}s linear #{start_time}s infinite;"
+          style << "    -webkit-animation: #{anim_prefix}#{flake + 1} #{speed}s linear #{start_time.round(1)}s infinite;"
           style << '  }'
+
+          start_time += start_interval
         end
 
-        # Space the snowflakes generally equally across the width of the
-        # container div.  Random distribution sometimes ends up with
-        # snowflakes clumped at one edge or the other.
-        flake_spacing = (100 - spread) / flakes.to_f
-        start_at = half_spread
-
+        # Declare each of the flake animations.
         flakes.times do |flake|
 
-          start_left = (start_at + rand(flake_spacing)).round
-          end_left = (start_left + rand(-half_spread..half_spread)).round
+          start_left = start_positions.pop
+
+          # Randomly choose where the snow will end its animation.  Prevent
+          # it from going outside of the container.
+          end_left = (start_left + rand(spread_range)).round
+          if end_left < POSITION_FLOOR
+            end_left = POSITION_FLOOR
+          elsif end_left > POSITION_CEIL
+            end_left = POSITION_CEIL
+          end
 
           style << "  @-webkit-keyframes #{anim_prefix}#{flake + 1} {"
-          style << "    0%   { top: 0%; left: #{start_left}%; }"
+          style << "    0%   { top: -3%; left: #{start_left}%; }"
           style << "    100% { top: 100%; left: #{end_left}%; }"
           style << '  }'
 
-          start_at += flake_spacing
         end
 
         style << '}'
@@ -164,6 +173,27 @@ module Inkcite
         html
 
       end
+
+      private
+
+      # Size constraints on the flakes.
+      FLAKE_SIZE_MIN = :'min-size'
+      FLAKE_SIZE_MAX = :'max-size'
+
+      # Speed constraints on the flakes.
+      FLAKE_SPEED_MIN = :'min-speed'
+      FLAKE_SPEED_MAX = :'max-speed'
+
+      # Opacity constraints.
+      FLAKE_OPACITY_MIN = :'min-opacity'
+      FLAKE_OPACITY_MAX = :'max-opacity'
+      OPACITY_FLOOR = 0.2
+      OPACITY_CEIL = 1.0
+
+      # Position min and max preventing snow flakes
+      # from leaving the bounds of the container.
+      POSITION_FLOOR = 0
+      POSITION_CEIL = 100
 
     end
 
