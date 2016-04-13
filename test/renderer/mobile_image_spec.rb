3e5584057f3aba7201a804d34eadcf78e1d6c5b6
Jeffrey D. Hoffman <jeffrey@inkceptional.com>
2016-04-12 21:56:16 -0500

    Another protocol-relative spec fix
    One last reference to //placeholdit full-qualified with the http protocol.

--- test/renderer/mobile_image_spec.rb
+++ test/renderer/mobile_image_spec.rb
@@ -17,7 +17,7 @@ describe Inkcite::Renderer::MobileImage do
   it 'substitutes a placeholder for a missing image of sufficient size' do
     @view.config[Inkcite::Email::IMAGE_PLACEHOLDERS] = true
     Inkcite::Renderer.render('{mobile-img src=inkcite-mobile.jpg height=100 width=300}{/mobile-img}', @view).must_equal('<span class="i01 img"></span>')
-    @view.media_query.find_by_klass('i01').to_css.must_equal('span[class~="i01"] { background-image:url("//placeholdit.imgix.net/~text?fm=jpg&h=100&txt=inkcite-mobile.jpg%0A%28300%C3%97100%29&txtsize=18&txttrack=0&w=300");height:100px;width:300px }')
+    @view.media_query.find_by_klass('i01').to_css.must_equal('span[class~="i01"] { background-image:url("http://placeholdit.imgix.net/~text?fm=jpg&h=100&txt=inkcite-mobile.jpg%0A%28300%C3%97100%29&txtsize=18&txttrack=0&w=300");height:100px;width:300px }')
   end
 
   it 'hides any images it wraps' do
