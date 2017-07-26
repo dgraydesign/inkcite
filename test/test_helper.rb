b30ccbe9f4246aa6f0a89d4f321503eedfa4c6c4
Jeffrey D. Hoffman <jeffrey@inkceptional.com>
2017-07-26 07:28:00 -0500

    New helper functions for asserting string containment and whether or not errors are present.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,17 @@ silence_warnings do
   require 'minitest/autorun'
   require 'inkcite'
 end
+
+def assert_contains string, expected_substring, *args
+  assert string.include?(expected_substring), *args
+end
+
+def assert_error view, *message
+  view.errors.any? { |e|
+    message.all? { |m| assert_contains(e, m) }
+  }.must_equal(true)
+end
+
+def assert_no_errors view
+  view.errors.must_be_nil
+end
